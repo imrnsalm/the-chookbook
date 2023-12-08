00796593f539c6af712a73dd78db2bfd0372e924
i8abyte <imransalim08@gmail.com>
2023-12-08 14:27:14 +1300

    feat: make the SQLite3 database queryable in Node.js with SQL

--- db.js
+++ db.js
@@ -1,18 +1,3 @@
-const recipes = [];
-
-recipes.push({
-    name: 'Soft Boiled Eggs',
-    ingredients: {
-        one: 'large egg(s), chilled'
-    },
-    instructions: {
-        one: 'Add 1 inch of water to a sauce pot, cover, and bring to a boil over high heat.',
-        two: 'Once boiling, add an egg (or however many you\'d like as long as they are in a single layer in a bottom of the pot), straight from the refridgerator into the pot. Replace the lid and let it continue to boil for exactly six minutes.',
-        three: 'After six minutes, remove the egg(s) from the pot and place them in an ice water bath or run under cool water until they are cool enough to handle. Peel, and enjoy!'
-    }
-});
-
-
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
@@ -29,7 +14,8 @@ db.exec(`
     );
     CREATE TABLE IF NOT EXISTS instruction (
         recipe_name TEXT NOT NULL,
-        instruction_step TEXT NOT NULL,
+        instruction_number INTEGER NOT NULL,
+        instruction_description TEXT NOT NULL,
         FOREIGN KEY (recipe_name)
             REFERENCES recipe (name)
     );`, (err) => {
@@ -47,12 +33,12 @@ db.exec(`
     INSERT INTO ingredient (recipe_name, ingredient_description)
         VALUES ("Soft Boiled Eggs", "large egg(s), chilled");
 
-    INSERT INTO instruction (recipe_name, instruction_step)
-        VALUES ("Soft Boiled Eggs", "1. Add 1 inch of water to a sauce pot, cover, and bring to a boil over high heat.");
-    INSERT INTO instruction (recipe_name, instruction_step)
-        VALUES ("Soft Boiled Eggs", "2. Once boiling, add an egg (or however many you'd like as long as they are in a single layer in a bottom of the pot), straight from the refridgerator into the pot. Replace the lid and let it continue to boil for exactly six minutes.");
-    INSERT INTO instruction (recipe_name, instruction_step)
-        VALUES ("Soft Boiled Eggs", "3. After six minutes, remove the egg(s) from the pot and place them in an ice water bath or run under cool water until they are cool enough to handle. Peel, and enjoy!");
+    INSERT INTO instruction (recipe_name, instruction_number, instruction_description)
+        VALUES ("Soft Boiled Eggs", 1, "Add 1 inch of water to a sauce pot, cover, and bring to a boil over high heat.");
+    INSERT INTO instruction (recipe_name, instruction_number, instruction_description)
+        VALUES ("Soft Boiled Eggs", 2, "Once boiling, add an egg (or however many you'd like as long as they are in a single layer in a bottom of the pot), straight from the refridgerator into the pot. Replace the lid and let it continue to boil for exactly six minutes.");
+    INSERT INTO instruction (recipe_name, instruction_number, instruction_description)
+        VALUES ("Soft Boiled Eggs", 3, "After six minutes, remove the egg(s) from the pot and place them in an ice water bath or run under cool water until they are cool enough to handle. Peel, and enjoy!");
         
 
     INSERT INTO recipe (name)
@@ -77,16 +63,16 @@ db.exec(`
     INSERT INTO ingredient (recipe_name, ingredient_description)
         VALUES ("Air Fryer Chicken Wings", "1/8 tsp cooking oil for greasing");
     
-    INSERT INTO instruction (recipe_name, instruction_step)
-        VALUES ("Air Fryer Chicken Wings", "1. Pat the chicken wings dry with a paper towel. Make sure to get as much moisture off the skin as possible.");
-    INSERT INTO instruction (recipe_name, instruction_step)
-        VALUES ("Air Fryer Chicken Wings", "2. Mix 1 teaspoon of salt with 1 teaspoon of baking powder per pound of chicken. Toss the dried chicken wings in this mixture until evenly coated. Place the wings on a rack set over a sheet pan and refridgerate for at least one hour.");
-    INSERT INTO instruction (recipe_name, instruction_step)
-        VALUES ("Air Fryer Chicken Wings", "3. Remove the wings from the fridge and pat them dry a second time. Mix the cornstarch, garlic powder, smoked paprika, onion powder, and black pepper called for in the recipe for every pound of chicken. Toss the wings in this mixture until thoroughly coated.");
-    INSERT INTO instruction (recipe_name, instruction_step)
-        VALUES ("Air Fryer Chicken Wings", "4. Grease the air fryer basket and preheat it to 300°F. Arrange the wings in a single layer, making sure they do not touch. Cook for 15 minutes.");
-    INSERT INTO instruction (recipe_name, instruction_step)
-        VALUES ("Air Fryer Chicken Wings", "5. Flip the wings over, raise the temperature of the air fryer to 450°F, and cook for 10 minutes. Use a meat thermometer to make sure the wings have an internal temperature of 165°F before serving.");
+    INSERT INTO instruction (recipe_name, instruction_number, instruction_description)
+        VALUES ("Air Fryer Chicken Wings", 1, "Pat the chicken wings dry with a paper towel. Make sure to get as much moisture off the skin as possible.");
+    INSERT INTO instruction (recipe_name, instruction_number, instruction_description)
+        VALUES ("Air Fryer Chicken Wings", 2, "Mix 1 teaspoon of salt with 1 teaspoon of baking powder per pound of chicken. Toss the dried chicken wings in this mixture until evenly coated. Place the wings on a rack set over a sheet pan and refridgerate for at least one hour.");
+    INSERT INTO instruction (recipe_name, instruction_number, instruction_description)
+        VALUES ("Air Fryer Chicken Wings", 3, "Remove the wings from the fridge and pat them dry a second time. Mix the cornstarch, garlic powder, smoked paprika, onion powder, and black pepper called for in the recipe for every pound of chicken. Toss the wings in this mixture until thoroughly coated.");
+    INSERT INTO instruction (recipe_name, instruction_number, instruction_description)
+        VALUES ("Air Fryer Chicken Wings", 4, "Grease the air fryer basket and preheat it to 300°F. Arrange the wings in a single layer, making sure they do not touch. Cook for 15 minutes.");
+    INSERT INTO instruction (recipe_name, instruction_number, instruction_description)
+        VALUES ("Air Fryer Chicken Wings", 5, "Flip the wings over, raise the temperature of the air fryer to 450°F, and cook for 10 minutes. Use a meat thermometer to make sure the wings have an internal temperature of 165°F before serving.");
     `, (err) => {
         if (err) {
             console.error(err.message);
@@ -95,4 +81,4 @@ db.exec(`
     }
 );
 
-module.exports = { db, recipes };
+module.exports = db;
