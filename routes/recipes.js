aacb014c465c86d4f666584d861f23b530a79e33
i8abyte <imransalim08@gmail.com>
2024-01-11 00:14:06 +1300

    feat: display the recipe data in JSON format

--- routes/recipes.js
+++ routes/recipes.js
@@ -4,36 +4,37 @@ const db = require('../db');
 
 /* GET recipes listing. */
 router.get('/', (req, res, next) => {
-    db.all('SELECT * FROM recipe', (err, rows) => {
+  db.all('SELECT * FROM recipe', (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    req.recipes = rows;
+
+    db.all('SELECT * FROM ingredient', (err, rows) => {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+      req.ingredients = rows;
+
+      db.all('SELECT * FROM instruction', (err, rows) => {
         if (err) {
             console.error(err.message);
             return;
         }
-        req.recipes = rows;
+        req.instructions = rows;
 
-        db.all('SELECT * FROM ingredient', (err, rows) => {
-            if (err) {
-                console.error(err.message);
-                return;
-            }
-            req.ingredients = rows;
-
-            db.all('SELECT * FROM instruction', (err, rows) => {
-                if (err) {
-                    console.error(err.message);
-                    return;
-                }
-                req.instructions = rows;
-
-                let data = {};
-                req.recipes.forEach(recipe => data[recipe.name] = { ingredients: [], instructions: [] });
-                req.ingredients.forEach(ingredient => data[ingredient.recipe_name].ingredients.push(ingredient.ingredient_description));
-                req.instructions.forEach(instruction => data[instruction.recipe_name].instructions.push(instruction.instruction_description));
-                
-                res.render('recipes', { title: 'Recipes', recipes: data });
-            });
-        });
+        let data = {};
+        req.recipes.forEach(recipe => data[recipe.name] = { ingredients: [], instructions: [] });
+        req.ingredients.forEach(ingredient => data[ingredient.recipe_name].ingredients.push(ingredient.ingredient_description));
+        req.instructions.forEach(instruction => data[instruction.recipe_name].instructions.push(instruction.instruction_description));
+        
+        res.header("Content-Type", 'application/json');
+        res.send(JSON.stringify(data, null, 2));
+      });
     });
+  });
 });
 
 module.exports = router;
