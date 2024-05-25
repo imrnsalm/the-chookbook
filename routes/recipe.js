b2eb86b915b026808087d5a9fe97158641f22ec1
i8abyte <imransalim08@gmail.com>
2024-05-26 05:20:27 +1200

    feat: add HTTP status codes and corresponding responses when requests are unsuccessful

--- routes/recipe.js
+++ routes/recipe.js
@@ -7,6 +7,8 @@ router.get('/all', (req, res, next) => {
   db.all('SELECT * FROM recipe', (err, rows) => {
     if (err) {
       console.error(err.message);
+      res.status(500);
+      res.send("500 Internal Server Error")
       return;
     }
     req.recipes = rows;
@@ -14,6 +16,8 @@ router.get('/all', (req, res, next) => {
     db.all('SELECT * FROM ingredient', (err, rows) => {
       if (err) {
         console.error(err.message);
+        res.status(500);
+        res.send("500 Internal Server Error")
         return;
       }
       req.ingredients = rows;
@@ -21,6 +25,8 @@ router.get('/all', (req, res, next) => {
       db.all('SELECT * FROM instruction', (err, rows) => {
         if (err) {
             console.error(err.message);
+            res.status(500);
+            res.send("500 Internal Server Error")
             return;
         }
         req.instructions = rows;
@@ -31,6 +37,7 @@ router.get('/all', (req, res, next) => {
         req.instructions.forEach(instruction => data[instruction.recipe_name].instructions[instruction.instruction_number] = instruction.instruction_description);
         
         res.header("Content-Type", 'application/json');
+        res.status(200);
         res.send(JSON.stringify(data, null, 2));
       });
     });
